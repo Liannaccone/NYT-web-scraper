de5b6402ed934475a78e6159f4acffa27c96d483
liannaccone <lciannaccone@gmail.com>
2018-03-19 21:29:30 -0700

    updating mongodb connection

--- server.js
+++ server.js
@@ -46,6 +46,12 @@ var MONGODB_URI = 'mongodb://heroku_gq2q3swn:o20vpnhpto1g8p2qdfaaevorhr@ds215019
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI, {});
 
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/nyt-scraper";
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI, {
+  useMongoClient: true
+});
+
 // load ROUTES
 require('./routes/routes.js')(app);
 
@@ -55,5 +61,5 @@ app.listen(PORT, function() {
 	console.log('App running on port', PORT)
 });
 
-// || "mongodb://localhost/nyt-scraper"
+
 // MONGODB_URI: mongodb://heroku_gq2q3swn:o20vpnhpto1g8p2qdfaaevorhr@ds215019.mlab.com:15019/heroku_gq2q3swn
